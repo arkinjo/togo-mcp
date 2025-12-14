a6e3f15d5d64de3265500bcfff5bcf5aad00d064
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2025-12-14 09:40:39 +0900

    Update rdf_portal.py

--- togo_mcp/rdf_portal.py
+++ togo_mcp/rdf_portal.py
@@ -217,7 +217,7 @@ SELECT DISTINCT ?graph WHERE {
 @mcp.tool(
         enabled=True,
         name="get_MIE_file",
-        description="Get the MIE file containing the ShEx schema, RDF and SPARQL examples of a specific RDF database. Use this before constructing any SPARQL queries for the database."
+        description="Get the MIE (Metadata Interoperability Exchange) file containing the ShEx schema, RDF and SPARQL examples of a specific RDF database. Use this before constructing any SPARQL queries for the database."
 )
 async def get_MIE_file(
     dbname: Annotated[str, Field(description=DBNAME_DESCRIPTION)]
